cb81f783fd4ecdc2f87e6a24ad54f1f6f60f4afa
Jiajun Ou <jiajun.ou1996@gmail.com>
2017-05-25 18:52:45 +1000

    push
    push

--- Instaflight/Instaflight/build/web/WEB-INF/classes/uts/wsd/Airline.java
+++ Instaflight/Instaflight/build/web/WEB-INF/classes/uts/wsd/Airline.java
@@ -42,6 +42,27 @@ public class Airline {
         }
         return null; // Login incorrect. Return null.
     }
+    public List<Flight> getByCity(String from, String destination){
+        
+        /*for (Flight flight : flights) {
+            if(flight.getOrigin().equals(from) && flight.getDestination().equals(destination))
+            {
+                flights.add(flight);
+            }
+        }*/
+        List<Flight> tempFlights = new ArrayList<Flight>();
+        for (int i = 0; i < flights.size(); i++) {
+            Flight flight = getFlightbyID(i);
+            if (flight.getOrigin().equals(from) && flight.getDestination().equals(destination)) {
+                tempFlights.add(flight);
+            }
+        }
+        return tempFlights;
+    }
+
+    public void setFlights(List<Flight> flights) {
+        this.flights = flights;
+    }
     public String getName(){
         return name;
     }
